efc4a2ac40ea6a3e4593c11bb791b2dbec31bf52
Abhishek Singh <abhisheksingh.nsut@gmail.com>
2020-12-26 23:01:14 +0530

    Updated Speech to Text Module

--- SpeechToText/speechtotext.py
+++ SpeechToText/speechtotext.py
@@ -1,10 +1,15 @@
 import time
 from recordaudio import recordvoice
-from predict import predict_audio   
+from predict import *   
+import warnings
+warnings.filterwarnings("ignore")
 def speech2text():
     audio=recordvoice()
-    query=predict_audio(audio)
+    with open('speech.wav','wb') as f:
+    	f.write(audio.get_wav_data())
+    query=predicts_audio('speech.wav')
     # if query !="None":
+#    print('h1'+query)
     return query
             
 if __name__ == '__main__':
